afb8f94fc2845198ebde2362d1426193b1b8a60f
Annika Hannig <annika@hannig.cc>
2023-06-27 19:21:27 +0200

    added datetime calculation

--- eris-db/src/accounting/datetime.rs
+++ eris-db/src/accounting/datetime.rs
@@ -2,6 +2,8 @@
 use chrono::{Datelike, NaiveDate};
 
 
+/// Get last month relative to now, aligned
+/// to the beginning of the month.
 pub fn last_month() -> NaiveDate {
     let now = chrono::Local::now().date_naive();
     now
@@ -11,6 +13,22 @@ pub fn last_month() -> NaiveDate {
         .unwrap()
         .with_day(1)
         .unwrap()
+}
+
+/// Get the number of months between two dates.
+/// This only accounts for full months. The days
+/// are irrelevant.
+pub trait CountMonths {
+    fn count_months(&self, other: &Self) -> u64;
+}
+
+/// Implement difference months for NaiveDate.
+impl CountMonths for NaiveDate {
+    fn count_months(&self, other: &Self) -> u64 {
+        let years = (other.year() - self.year()).abs() as i64;
+        let months = (other.month() as i32 - self.month() as i32) as i64;
+        (years * 12 + months) as u64
+    }
 }
 
 
@@ -26,6 +44,19 @@ mod tests {
         let date = last_month();
         println!("last: {:?}", date);
     }
+    
+    #[test]
+    fn test_diff_month() {
+        let d1 = NaiveDate::from_ymd_opt(2022, 11, 15).unwrap();
+        let d2 = NaiveDate::from_ymd_opt(2022, 12, 20).unwrap(); 
+        let d3 = NaiveDate::from_ymd_opt(2023, 1, 1).unwrap();
+        let d4 = NaiveDate::from_ymd_opt(2023, 2, 2).unwrap();
+
+        assert_eq!(d1.count_months(&d2), 1);
+        assert_eq!(d1.count_months(&d3), 2);
+        assert_eq!(d1.count_months(&d4), 3);
+    }
+
 }
 
 
